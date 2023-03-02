091ee79b2867772f0d121b5949ff70bf16ad7254
DavidRose <DavidRose331@Gmail.com>
2023-03-02 09:08:36 -0600

    git remote add origin git@github.com:DavidRose1989/FizzBuzzChallenge.git

---
+++ FizzBuzz Excercise.cs
@@ -0,0 +1,30 @@
+namespace azarias
+{
+public static class Solution
+{
+  
+  public string FizzBuzz(int num)
+  {
+  var answer = "";
+
+    if (num % 3 == 0)
+  {
+    answer = "fizz";
+  }
+
+  if (num % 5 == 0)
+  {
+     answer = "buzz";
+  }
+
+  if (num % 3 == 0 && num % 5 == 0)
+  {
+ answer = "fizzbuzz";
+}
+
+return answer;
+    
+
+
+  
+}
